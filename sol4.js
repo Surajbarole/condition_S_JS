d7581fdffd328b9c7279a517971b0861f9d67f43
Suraj Barole <103420352+Surajbarole@users.noreply.github.com>
2023-02-16 07:57:41 +0000

    Initial commit

---
+++ sol4.js
@@ -0,0 +1,11 @@
+// Problem 4: Given stored username and password and input username and password, Print if the user can login or not.
+let reg_user="abc@gmail.com";
+let reg_pass="123456@"
+
+let login_user="abc@gmail.com"
+let login_pass="123456@";
+if(reg_user==login_user&&reg_pass==login_pass){
+  console.log("Login success")
+}else{
+  console.log("Invalid think")
+}
